8adf3ac68121d4dc19e7acca5162c0b59a6b9fca
Shriya Bhat <118034452+Shriya-20@users.noreply.github.com>
2024-07-01 14:04:02 +0530

    length of Shortest Common Supersequence tc:O(mn) - LCS

---
+++ ShortestCommonSuperSequence(LCS).cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int LCS(string a, string b, int m, int n) {
+	int t[m + 1][n + 1];
+
+	for (int i = 0; i <= m; i++)
+		for (int j = 0; j <= n; j++)
+			if (i == 0 || j == 0)
+				t[i][j] = 0;
+
+	for (int i = 1; i <= m; i++){
+		for (int j = 1; j <= n; j++){
+			if (a[i - 1] == b[j - 1]) // when last character is same
+				t[i][j] = 1 + t[i - 1][j - 1];
+			else // when last character is not same -> pick max
+				t[i][j] = max(t[i][j - 1], t[i - 1][j]);
+		}
+	}
+    return t[m][n];
+}
+int ShortestSuperSequence(string s1,string s2,int m,int n){ //returns length of shortest super sequence
+    return m+n-LCS(s1,s2,m,n);
+}
+int main(){
+    string s1="abcd";
+    string s2="xycd";
+    int m=s1.size();
+    int n=s2.size();
+    cout<<ShortestSuperSequence(s1,s2,m,n);
+}
